0b41c5803a8b664b0bfa4f59b01253bad2338d66
Ricardo Coelho <ricardo.rcj@gmail.com>
2020-07-19 04:26:02 -0300

    feat: entradas em /etc/hosts alteracao nos arquivos de hosts das VMs via script criação de variaveis para configuracao de dominio e ip
     Mudanças a serem submetidas:
    	modified:   script.sh

--- script.sh
+++ script.sh
@@ -7,3 +7,14 @@
     chmod 400 /root/.ssh/id_rsa*
     cat /root/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
 
+# edicao do arquivo /etc/hosts
+IP='192.168.0.'
+domain='.sgp.local'
+HOSTS=$(head -n7 /etc/hosts)
+echo -e "$HOSTS" > /etc/hosts
+echo $IP'160 acesso-01'$domain >> /etc/hosts
+echo $IP'110 automation-01'$domain >> /etc/hosts
+echo $IP'120 compliance-01'$domain >> /etc/hosts
+echo $IP'130 container-01'$domain >> /etc/hosts
+echo $IP'140 scm-01'$domain >> /etc/hosts
+echo $IP'150 log-01'$domain >> /etc/hosts
